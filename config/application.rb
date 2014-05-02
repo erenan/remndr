4b19966fe5114ad1b0664ec2f579e6b8026a8317
eRenan <yuangao@eRenans-MacBook-Air.local>
2014-05-01 22:53:01 -0700

    more db changes

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
 
 module Rmndr
   class Application < Rails::Application
