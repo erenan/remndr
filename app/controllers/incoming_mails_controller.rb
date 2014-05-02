45f9970d040ca9b2d215a7e29c2fca435f2f9839
eRenan <yuangao@eRenans-MacBook-Air.local>
2014-05-01 23:27:07 -0700

    playing with receiving mail

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -2,8 +2,8 @@ class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
  def create
- Rails.logger.info params
-    message = Message.new(
+#Rails.logger.info params
+    @message = Emails.new(
       :to => params[:envelope][:to],
       :from => params[:envelope][:from],
       :subject => params[:headers]['Subject'],
@@ -11,9 +11,11 @@ class IncomingMailsController < ApplicationController
     )
     if message.save
       render :text => 'Success', :status => 200
+      redirect_to @message
     else
       render :text => message.errors.full_messages, :status => 422, :content_type => Mime::TEXT.to_s
     end
+  
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502040851) do
+ActiveRecord::Schema.define(version: 20140502060856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "emails", force: true do |t|
+    t.string   "to"
+    t.string   "from"
+    t.string   "subject"
+    t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "links", force: true do |t|
     t.string   "title"
