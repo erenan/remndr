ab9736eccf27b83eb53c7dcb1e30bae1baab8f44
eRenan <edgar@edgarrivera.com>
2014-05-11 23:42:03 -0700

    new version with Bootstrap V1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rmndr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.assets.precompile=true
-  #config.assets.precompile += %w( *.css *.js )
+  #config.assets.precompile=true
+  config.assets.precompile += %w( *.scss *.js )
   # Code is not reloaded between requests.
   config.cache_classes = true
 
