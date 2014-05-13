52f1f6ef7f9249e388ecd908d188edf42735dee6
eRenan <edgar@edgarrivera.com>
2014-05-12 22:59:22 -0700

    added categories and redesigned UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rmndr::Application.routes.draw do
   root 'main#index'
   resources :links
   #resources :reminders #Currently not used in V1.0; base for reminder function in V2.0
+  get 'categories/show_all' => 'categories#show_all'
   resources :categories
   post '/emails' => 'emails#create'
   get 'main/links' => 'main#links'
   get 'main/reminders' => 'main#reminders'
-  get 'categories/show_all' => 'categories#show_all'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
