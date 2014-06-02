253253c5eab3a0b9b8fcb34e47c31b643dd18cb3
eRenan <edgar@edgarrivera.com>
2014-06-01 22:55:18 -0700

    sundry changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rmndr::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+
+
+=begin THIS WILL BE THE EMAIL IMPLEMENTATION
+ActionMailer::Base.server_settings = {
+   :address => "smtpout.secureserver.net",
+   :port => 25,
+   :domain => "remnd.it",
+   :authentication => :login,
+   :user_name => "just@remnd.it",
+   :password => "jinxed62!",
+}
+=end
